758ca094580830b87aca61bf3eafaaa6dfe3622b
ayesha qamar <qamaraye@sheridancollege.ca>
2021-10-11 02:36:07 -0400

    added getters and setters for marks

--- src/student/Student.java
+++ src/student/Student.java
@@ -12,7 +12,9 @@ package student;
 public class Student {
        private String name;
        private String address;
-
+       private double marks;
+       
+       
     public double getMarks() {
         return marks;
     }
@@ -20,7 +22,7 @@ public class Student {
     public void setMarks(double marks) {
         this.marks = marks;
     }
-       private double marks;
+       
 
     public String getAddress() {
         return address;
